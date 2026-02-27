32fee777c76206a1be4bcb81111a08c9042e8115
Ahmad-Amireh <aamerh21@gmail.com>
2026-02-27 21:59:43 +0300

    Add post schemas for request validation and responses

---
+++ schemas.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel, Field, ConfigDict
+
+class PostBase (BaseModel):
+    author: str = Field(min_length=1, max_length= 20)
+    title: str = Field(min_length=1, max_length=100)
+    content: str = Field(min_length=1)
+    
+class PostCreate(PostBase):
+    pass
+
+class PostResponse(PostBase):
+    model_config = ConfigDict(from_attributes= True)
+
+    id: int
+    date_posted: str
+
