fc6a4bff3d3fe1f496c9cb5c78e34868fd59f595
Ahmad-Amireh <aamerh21@gmail.com>
2026-02-28 16:10:33 +0300

    Add User pydantic schemas

--- schemas.py
+++ schemas.py
@@ -1,4 +1,19 @@
-from pydantic import BaseModel, Field, ConfigDict
+from pydantic import BaseModel, Field, ConfigDict, EmailStr
+from datetime import datetime
+
+
+
+class UserBase(BaseModel):
+    name:str = Field(min_length=1, max_length=20)
+    email: EmailStr = Field(max_length=120)
+
+class UserCreate(BaseModel): 
+    pass
+
+class UserResponse(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    
+    id:str 
 
 class PostBase (BaseModel):
     author: str = Field(min_length=1, max_length= 20)
